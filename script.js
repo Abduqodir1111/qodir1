67925a572e8c96db43a02a003a3165889d0a9eb2
Abduqodir1111 <qodirabdunabiyev1111@gmail.com>
2025-05-22 20:46:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', () => {
+  const introScreen = document.getElementById('intro-screen');
+  const mainContent = document.getElementById('main-content');
+  const letters = document.querySelectorAll('#animated-text .letter');
+
+  // Добавляем класс с анимацией и задержкой для каждой буквы
+  letters.forEach((letter, i) => {
+    letter.style.animationDelay = `${i * 0.1}s`;
+    letter.classList.add('slide-bck-top');
+  });
+
+  // Скрываем заставку через 3 секунды (или чуть позже, чтобы анимация успела проиграться)
+  setTimeout(() => {
+    introScreen.style.opacity = '0';
+    introScreen.style.pointerEvents = 'none';
+
+    setTimeout(() => {
+      introScreen.style.display = 'none';
+      mainContent.classList.add('show');
+    }, 1000);
+  }, letters.length * 100 + 2000); // время анимации букв + небольшой запас
+});
