41d5c0976828d1ee6300021169370a9e9064d955
Abduqodir1111 <qodirabdunabiyev1111@gmail.com>
2025-05-30 23:48:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const openModalBtn = document.getElementById("openModalBtn");
+const modal = document.getElementById("bookingModal");
+const closeModalBtn = document.getElementById("closeModalBtn");
+
+openModalBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.style.display = "block";
+});
+
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
